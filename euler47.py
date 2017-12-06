36c6431179ea1d7409947da8e7333bc0b5e2d8c7
jessekrubin <jessekrubin@gmail.com>
2017-12-06 16:10:40 -0500

    herm herm

---
+++ euler47.py
@@ -0,0 +1,18 @@
+def prime_factors_set(n):
+    i = 2
+    factors = []
+    while(i * i < n + 1):
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return set(factors)
+
+
+print(prime_factors_set(52))
+print(prime_factors_set(700))
+print(len(prime_factors_set(52)))
+print(len(prime_factors_set(700)))
