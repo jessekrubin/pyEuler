11e2c519b9a9e5dff09b9ee3fd639a2cab850963
jessekrubin <jessekrubin@gmail.com>
2018-01-01 23:13:56 -0500

    chilling

--- not_done/euler_037.py
+++ not_done/euler_037.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Jesse and Graham Rubin
+
 """
 Truncatable primes
 Problem 37 
 The number 3797 has an interesting property. Being prime itself, it is possible
 to continuously remove digits from left to right, and remain prime at each
 stage: 3797, 797, 97, and 7. Similarly we can work from right to left:
- 3797, 379, 37, and 3.
+3797, 379, 37, and 3.
 
 Find the sum of the only eleven primes that are both truncatable from left to
 right and right to left.
 
 NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 """
+
 import functools
 import math
+
 
 @functools.lru_cache(maxsize=None)
 def is_prime(n):
@@ -28,6 +31,7 @@ def is_prime(n):
         return False
     return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
 
+
 def trunk_prime(n):
     if n == 2 or n == 3 or n == 5 or n == 7:
         return False
@@ -35,9 +39,9 @@ def trunk_prime(n):
         return False
     digs = [i for i in str(n)]
     for i in range(1, len(digs)):
-        right= int("".join(digs[i:]))
+        right = int("".join(digs[i:]))
         # print(right)
-        p = 1-i
+        p = 1 - i
         left = int("".join(digs[:i]))
         if not is_prime(right) or not is_prime(left):
             return False
@@ -49,7 +53,7 @@ def trunk_prime(n):
 print(trunk_prime(3797))
 
 
-primes = [ i for i in range(2, 100000) if trunk_prime(i) ]
+primes = [i for i in range(2, 100000) if trunk_prime(i)]
 print((primes))
 print(len(primes))
 print(max(primes))
--- not_done/euler_086.py
+++ not_done/euler_086.py
@@ -3,9 +3,10 @@
 # Jesse and Graham Rubin
 """
 Path sum: two ways
-Problem 81 
+Problem 81
 In the 5 by 5 matrix below, the minimal path sum from the top left to the
-bottom right, by only moving to the right and down, is indicated in bold red and is equal to 2427.
+bottom right, by only moving to the right and down, is indicated in bold red
+and is equal to 2427.
 
 [[131, 673, 234, 103,  18],
  [201,  96, 342, 965, 150],
@@ -13,10 +14,18 @@ bottom right, by only moving to the right and down, is indicated in bold red and
  [537, 699, 497, 121, 956],
  [805, 732, 524,  37, 331]]
 
-Find the minimal path sum, in matrix.txt (right click and "Save Link/Target As..."), a 31K text file containing a 80 by 80 matrix, from the top left to the bottom right by only moving right and down.
+Find the minimal path sum, in matrix.txt, a 31K text file containing a 80 by
+80 matrix, from the top left to the bottom right by only moving right and
+down.
 """
+import os
+from os import path
 
 g1 = [[131, 673, 234, 103, 18], [201, 96, 342, 965, 150],
       [630, 803, 746, 422, 111], [537, 699, 497, 121,
                                   956], [805, 732, 524, 37, 331]]
 
+cur_dir = os.getcwd() + '\\not_done\\'
+with open(path.join(cur_dir, r'text_files\p081_matrix.txt'), 'r') as file:
+    ls = file.readlines()
+print(ls)
