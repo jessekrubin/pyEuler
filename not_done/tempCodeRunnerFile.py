1561c3663df0386bb6cfeb0fb58889a88251fb15
jessekrubin <jessekrubin@gmail.com>
2018-01-06 13:45:03 -0500

    cleanup

--- not_done/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-    if a == c != 0:
-        return og_fracking == float(b / d)
-    if a == d:
-        return og_fracking == float(b / c)
-    if b == d:
-        return og_fracking == float(a / c)
- 
