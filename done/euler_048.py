3f5239abd68b760338c5a264a8928304621b78d0
jessekrubin <jessekrubin@gmail.com>
2018-01-10 18:27:11 -0500

    adios

---
+++ done/euler_048.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Jesse Rubin
+"""
+Self powers
+Problem 48
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+"""
+
+def power_series(number: int):
+    series_sum = 0
+    for i in range(1, number+1):
+        series_sum += i**i
+    return series_sum
+
+series_sum_1000 = power_series(1000) % 10000000000
+print(f"Answer: {series_sum_1000}")
+
---
+++ euler_056.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Jesse Rubin
+"""
+Powerful digit sum
+Problem 56
+A googol (10100) is a massive number: one followed by one-hundred zeros;
+100100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+
+Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
+"""
