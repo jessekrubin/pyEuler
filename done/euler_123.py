631c39ef25fcf6e0aa8bb22dd25f3e4f03e5661a
jessekrubin <jessekrubin@gmail.com>
2018-03-13 19:47:38 -0400

    123

---
+++ done/euler_123.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Jesse Rubin
+"""
+Prime square remainders
+Problem 123
+Let pn be the nth prime: 2, 3, 5, 7, 11, ..., and let r be the remainder when
+(pn−1)n + (pn+1)n is divided by pn2.
+
+For example, when n = 3, p3 = 5, and 43 + 63 = 280 ≡ 5 mod 25.
+
+The least value of n for which the remainder first exceeds 109 is 7037.
+
+Find the least value of n for which the remainder first exceeds 1010.
+"""
+
+
+from lib.octopus_prime import prime_sieve_gen
+from lib.decorations import tictoc
+
+@tictoc()
+def psr(remainder_max):
+    for n, p in enumerate(prime_sieve_gen()):
+        rem = 2*(n+2)*p
+        if rem > remainder_max:
+            return n
+
+print((psr(1000000000)))
+print((psr(10000000000)))
+
+
+
---
+++ not_done/euler_108.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Jesse Rubin
+"""
+Diophantine reciprocals I
+Problem 108
+In the following equation x, y, and n are positive integers.
+
+1/x+1/y=1/n
+
+For n = 4 there are exactly three distinct solutions:
+
+1/5+1/20 = 1/4
+1/6+1/12 = 1/4
+1/8+1/8  = 1/4
+What is the least value of n for which the number of distinct solutions exceeds one-thousand?
+
+NOTE: This problem is an easier version of Problem 110; it is strongly advised that you solve this one first.
+"""
+from math import sqrt
+
+from itertools import count
+
+
+def count_recips(n):
+    total = 1 # because everything has at least one
+    print(n)
+    for i in range(n+1, 2*n):
+        for j in count(2*n):
+            print(i, j)
+            if (i*j ==  i*n + j*n):
+                total += 1
+            elif (i*j > i*n + j*n):
+                # print(i*j)
+                # print(i*n)
+                # print(n*j)
+                break
+                # return total
+            if j > 100:
+                return total
+
+print(count_recips(4))
+
+# def p108(limit):
+#     ddd = {}
+#     for x in count(1):
+#         # if x > limit**2:
+#         #     break
+#         for y in range(1, x):
+#             mulled = x * y
+#             added = x + y
+#             n = mulled // added
+#             modded = mulled % added
+#             # print(x, y, n)
+#             if modded == 0:
+#                 if n in ddd:
+#                     ddd[n] += 1
+#                     if ddd[n] > limit-1:
+#                         print(ddd)
+#                         return n
+#                     # print(ddd[n], "HAYHAY")
+#                 else:
+#                     ddd.setdefault(n, 1)
+#
+#         # print(x, len(ddd))
+#
+#             # if x>20:
+#             #     return ddd
+# print(p108(1000))
+#
+
