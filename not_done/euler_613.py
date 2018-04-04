2d2938b91bcf9317f18eaa5e5dfbc833445404ce
jessekrubin <jessekrubin@gmail.com>
2018-04-04 19:03:14 -0400

    saved 613

--- not_done/euler_613.py
+++ not_done/euler_613.py
@@ -21,7 +21,6 @@ Give your answer rounded to 10 digits after the decimal point.
 """
 
 from math import pi
-from multiprocessing import Pool
 from statistics import mean
 
 from tqdm import tqdm
@@ -29,12 +28,17 @@ from tqdm import tqdm
 from lib.vfunk import angle
 
 
-def get_vecs(tup):
-    c1 = (400, 0)
-    c2 = (0, 300)
-    v1 = (c1[0] - tup[0], c1[1] - tup[1])
-    v2 = (c2[0] - tup[0], c2[1] - tup[1])
-    return v1, v2
+c1 = 4000
+c2 = 3000
 
-print(get_vecs((0, 0)))
+def point_prob(tup):
+    v1 = (c1 - tup[0],  -tup[1])
+    v2 = (- tup[0], c2 - tup[1])
+    return (angle(v1, v2)/(2*pi))
 
+
+if __name__ == '__main__':
+    points = [[i, j] for i in range(0, c1) for j in range(0, c2)]
+    probs = list(tqdm(map(point_prob, points), total=len(points)))
+    answer = mean(probs)
+    print("probability: {}".format(answer))
