1ae24dde147d8cddf4f7c966fd711cb013252036
jessekrubin <jessekrubin@gmail.com>
2018-04-24 19:00:22 -0400

    exp sign

--- no_cigar/euler_187.py
+++ no_cigar/euler_187.py
@@ -10,7 +10,7 @@ A composite is a number containing at least two prime factors. For example,
 There are ten composites below thirty containing precisely two, not
 necessarily distinct, prime factors: 4, 6, 9, 10, 14, 15, 21, 22, 25, 26.
 
-How many composite integers, n < 108, have precisely two, not necessarily
+How many composite integers, n < 10**8, have precisely two, not necessarily
 distinct, prime factors?
 """
 
