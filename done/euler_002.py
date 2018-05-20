2ccbf4a50eb6d56ba7cce9535e3c96907f717fd0
jessekrubin <jessekrubin@gmail.com>
2018-05-20 15:16:40 -0400

    herm

--- done/euler_002.py
+++ done/euler_002.py
@@ -17,12 +17,11 @@ from lib.maths import fib_r
 
 
 def p002(upper_bound=4000000):
-    return sum(
-            n for n in (fib_r(i) for i in range(40))
-            if n % 2 == 0 and n < upper_bound + 1
-            )
+    return sum(n for n in (fib_r(i) for i in range(40))
+               if n%2 == 0
+               and n < upper_bound+1)
 
 
 if __name__ == '__main__':
-    even_fibs_below_4_mil = p002()
-    print("Even fib_r numbers below 4 million: {}".format(even_fibs_below_4_mil))
+    ANSWER = p002()
+    print("Even fibonacci numbers less than 4,000,000: {}".format(ANSWER))
