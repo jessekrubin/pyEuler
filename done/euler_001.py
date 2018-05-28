83771f86e3572a94a39358ced20adc1b3aa929d0
jessekrubin <jessekrubin@gmail.com>
2018-05-28 14:07:02 -0400

    oy vey

--- done/euler_001.py
+++ done/euler_001.py
@@ -10,13 +10,18 @@ get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
+def multiples_lt(lim, div):
+    mul = lim // div
+    return ((mul*mul*div)+(mul*div)) // 2
+
+def multiples_o_3_n_5(upper_bound):
+    return multiples_lt(upper_bound, 3) + multiples_lt(upper_bound, 5) - multiples_lt(upper_bound, 15)
+
 
 def p001(upper_bound=1000):
-    return sum(i for i in range(upper_bound) if (i%3 == 0) or (i%5 == 0))
+    return multiples_o_3_n_5(upper_bound - 1)
 
 
 if __name__ == '__main__':
     answer = p001()
     print("Sum: {}".format(answer))  # Sum: 233168
-
-
